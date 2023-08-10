eb237e99de9ec611283bb8401e66bdfe2a9694fe
Beatriz <beatriz.terradas@gmail.com>
2023-08-10 16:50:02 -0300

    using debug e logRoles function

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, logRoles } from '@testing-library/react';
 import { Skills } from './skills';
 
 describe('Skills', () => {
@@ -37,14 +37,17 @@ describe('Skills', () => {
   })
 
   test('Start Learning button is eventualy displayed', async () => {
-    render(<Skills skills={skills} />)
+    const view = render(<Skills skills={skills} />)
+    logRoles(view.container)
 
+    // screen.debug()
     const startLearningButton = await screen.findByRole('button', {
       name: 'Start Learning'
     }, 
     {
       timeout: 2000
     })
+    // screen.debug()
     expect(startLearningButton).toBeInTheDocument()
   })
 })
